5c17f6143cca66c34817372a8d07d733aa7dd8d9
enis nishori <en46627@ubt-uni.net>
2023-09-10 20:57:35 +0200

    added mongo db

--- backend-app/pages/index.js
+++ backend-app/pages/index.js
@@ -1,11 +1,19 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
-  return (
-    <>
+
+  const { data: session } = useSession()
+
+  if (!session) {
+    return (
       <div className='bg-blue-900 w-screen h-screen flex items-center'>
         <div className="text-center w-full">
-          <button className="bg-white p-4 rounded-lg">Login with Google</button>
+          <button onClick={() => signIn('google')} className="bg-white p-4 rounded-lg">Login with Google</button>
         </div>
       </div>
-    </>
+    );
+  }
+  return (
+    <div>Logged in {session.user.email} </div>
   )
 }
