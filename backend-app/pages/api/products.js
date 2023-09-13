f42c2039e842a2a899928b360567747b545eda23
enis nishori <en46627@ubt-uni.net>
2023-09-13 17:22:28 +0200

    created successfully product

--- backend-app/pages/api/products.js
+++ backend-app/pages/api/products.js
@@ -1,6 +1,17 @@
-export default function handle(req, res) {
-    const { method } = req;
-    if (method === 'POST') {
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
+export default async function handle(req, res) {
+    const { method } = req;
+
+    //making sure that we have a connection to the database
+    await mongooseConnect()
+
+    if (method === 'POST') {
+        const { title, description, price } = req.body
+        const productDoc = await Product.create({
+            title, description, price
+        })
+        res.json(productDoc)
     }
 }
