263568e729bf265d0363152313edab08421a3127
enis nishori <en46627@ubt-uni.net>
2023-09-17 13:12:16 +0200

    when added productForm component we cleaned the new.js file

--- backend-app/pages/products/new.js
+++ backend-app/pages/products/new.js
@@ -1,58 +1,11 @@
 import Layout from "@/components/Layout";
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import ProductFrom from "@/components/ProductFrom";
 
 export default function NewProduct() {
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [price, setPrice] = useState('')
-    const [goToProducts, setGoToProducts] = useState(false)
-    const router = useRouter()
-
-    async function createProduct(ev) {
-        ev.preventDefault();
-        const data = { title, description, price }
-        await axios.post('/api/products', data)
-
-        setGoToProducts(true)
-    }
-
-    if (goToProducts) {
-        router.push('/products')
-    }
-
     return (
         <Layout>
-            <form onSubmit={createProduct}>
-                <h1>Add New Product</h1>
-                <label>Product Name</label>
-                <input
-                    type="text"
-                    placeholder="Product Name"
-                    value={title}
-                    onChange={ev => setTitle(ev.target.value)}
-                />
-
-                <label>Description</label>
-                <textarea
-                    placeholder="Description"
-                    value={description}
-                    onChange={ev => setDescription(ev.target.value)}
-                />
-                <label>Price (in Euro)</label>
-                <input
-                    type="number"
-                    placeholder="Price"
-                    value={price}
-                    onChange={ev => setPrice(ev.target.value)}
-                />
-                <button
-                    type="submit"
-                    className="btn-primary">
-                    Save the Product
-                </button>
-            </form>
+            <h1>New Product</h1>
+            <ProductFrom />
         </Layout>
     )
 }
